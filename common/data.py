d68b4bb5bd6c726584b622dedf6ad180f722a9d1
Poolce <poolce@list.ru>
2024-04-06 18:39:27 +0300

    gg

--- common/data.py
+++ common/data.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass, asdict
 from typing import List
+from math import log
+from os.path import join, abspath
+from os import getcwd
 
 
 @dataclass
@@ -11,44 +14,103 @@ class Metrics:
     recip_TDP: float
 
 
+@dataclass
+class Metrics2D:
+    _2D_G3Dmark2: float
+    _2D_G2Dmark2: float
+    _2D_recip_price2: float
+    _2D_recip_TDP2: float
+    _2D_G3Dmark_G2Dmark:float
+    _2D_G3Dmark_recip_price:float
+    _2D_G3Dmark_recip_TDP:float
+    _2D_G2Dmark2_recip_price:float
+    _2D_G2Dmark2_recip_TDP:float
+    _2D_recip_price_recip_TDP:float
+
+@dataclass
+class Metrics3D:
+    _3D_G3G2price: float
+    _3D_G3G2TDP: float
+    _3D_G3priceTDP: float
+    _3D_G2priceTDP: float
+
+@dataclass
+class Metrics4D:
+    _4D_ALL_CHARACTERS: float
+
+
 @dataclass   
 class Stats:
-    m1: float
-    m2: float
-    m3: float
-    m4: float
-    m5: float
+    stats_list : List
 
 
 class GPU:
     name: str
-    stat: Stats
+    stat: list
     metrics: Metrics
+    metrics2D: Metrics2D
+    metrics3D: Metrics3D
 
-    def __init__(self, _name, _stat, _metr) -> None:
+    def __init__(self, _name, _stat, _metr:Metrics) -> None:
         self.name = _name
         self.stat = _stat
         self.metrics = _metr
+        self.metrics2D = Metrics2D(_metr.G3Dmark**2, _metr.G2Dmark**2, _metr.recip_price**2, _metr.recip_TDP**2,
+                                   _metr.G3Dmark*_metr.G2Dmark, _metr.G3Dmark*_metr.recip_price, _metr.G3Dmark*_metr.recip_TDP,
+                                   _metr.G2Dmark*_metr.recip_price, _metr.G2Dmark*_metr.recip_TDP,
+                                   _metr.recip_price*_metr.recip_TDP)
+        
+        self.metrics3D = Metrics3D(_metr.G3Dmark*_metr.G2Dmark* _metr.recip_price,
+                                   _metr.G3Dmark*_metr.G2Dmark*_metr.recip_TDP,
+                                   _metr.G3Dmark* _metr.recip_price*_metr.recip_TDP,
+                                   _metr.G2Dmark* _metr.recip_price*_metr.recip_TDP)
+        
+        self.metrics4D = Metrics4D(_metr.G3Dmark*_metr.G2Dmark* _metr.recip_price*_metr.recip_TDP)
 
     def get_metrics(self) -> dict:
         return asdict(self.metrics)
     
+    def get_2dmetrics(self) -> dict:
+        return asdict(self.metrics2D)
+    
     def get_stats(self) -> dict:
-        return asdict(self.stat)
+        return {f'm_{i}' : self.stat[i] for i, _ in enumerate(self.stat)}
 
     def get_name(self):
         return self.name
+    
+    def get_united_metrics(self) -> dict:
+        s = {'J': self.get_Jval()}
+        s.update(asdict(self.metrics))
+        s.update(asdict(self.metrics2D))
+        s.update(asdict(self.metrics3D))
+        s.update(asdict(self.metrics4D))
+        return s
+    
+    def get_Jval(self) -> float:
+        stats_arr = self.stat
+        res = 0
+        for i in range(len(stats_arr)):
+            if i == 0: continue
+            if stats_arr[i] != 0 and stats_arr[i-1]:
+                res += log(1 + ((stats_arr[i]-stats_arr[i-1])/stats_arr[i-1]))
+                
+        return res/len(stats_arr)
 
 
-def get_data(csv_file_path) -> List[GPU]:
-    with open(csv_file_path, 'r') as file:
-        lines = file.readlines()
+
+
+def GetData():
+    with open(join(abspath(join(__file__, '..')), 'data.csv'), 'r') as data:
+        lines = data.readlines()
+    
     res = []
     for line in lines[1:]:
-        d = line.split(',')
-        res.append(
-            GPU(d[0],
-            Stats(float(d[1]), float(d[2]), float(d[3]), float(d[4]), float(d[5])),
-            Metrics(float(d[6]), float(d[7]), 1/float(d[8]), 1/float(d[9])))
-        )
+        line = line[:-1]
+        spl = line.split(',')
+        print(spl)
+        metr = Metrics(float(spl[1]), float(spl[2]), float(spl[3]), float(spl[4]))
+        stat = [float(i) for i in spl[5:]]
+        res.append(GPU(spl[0], stat, metr))
+        
     return res
--- main.py
+++ main.py
@@ -1,22 +1,56 @@
 import sys
 from matplotlib import pyplot as plt
 import pandas as pd
+import seaborn as sns
+import numpy 
 
-from common.data import get_data
+from common.data import GetData
 
 def main():
-    data = get_data('./BenchDataset.csv')
+    data = GetData()
+    statsDF = pd.DataFrame([dict(i.get_stats()) for i in data]).T
+    statsDF.columns = [i.get_name() for i in data]
+    print(statsDF)
+    # statsDF.plot(logy=False)
+    # plt.show()
+    # statsDF.plot(logy=False, legend=False)
+    # plt.show()
+    # statsDF.plot(logy=True, legend=False)
+    # plt.show()
 
-    statsDF = pd.DataFrame([i.get_stats() for i in data]).T
-    print(statsDF[1])
-    statsDF.plot(logy=False)
+    # metricsDF= pd.DataFrame([i.get_metrics() for i in data])
+
+    # corr = metricsDF.corr()
+    # sns.heatmap(corr, annot=True, cmap='vlag',
+    #         xticklabels=corr.columns.values,
+    #         yticklabels=corr.columns.values)
+    # plt.show()
+
+    unitedDF = pd.DataFrame([i.get_united_metrics() for i in data])
+
+    corr = unitedDF.corr()
+    sns.heatmap(corr, annot=True, cmap='vlag',
+            xticklabels=corr.columns.values,
+            yticklabels=corr.columns.values)
     plt.show()
 
-    metricsDF= pd.DataFrame([i.get_metrics() for i in data])
-    print(metricsDF)
+    classification_table = []
+    for i in [(gpu.get_name(), gpu.get_Jval()) for gpu in data]:
+        row = [i[0]]
+        for j in [(gpu.get_name(), gpu.get_Jval()) for gpu in data]:
+            if j[0] == i[0]:
+                row.append(0)
+                continue
+            if i[1] > j[1]:
+                row.append(1)
+            else:
+                row.append(-1)
+        classification_table.append(row)
 
+    classificationDF = pd.DataFrame(classification_table, columns=['name']+[gpu.get_name() for gpu in data])
+    classificationDF.to_csv('ClassificationData.csv')
 
-
+    
 
 if __name__ == '__main__':
     sys.exit(main())
